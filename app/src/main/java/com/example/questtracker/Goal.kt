6d29686c09fcf8d4c8dd61c2b5c64cff7676a003
Tri Nghia Nguyen <tringhianguyen@macbookair.lan>
2025-04-23 13:17:01 -0400

    Add data class for todo task, goal, and date

---
+++ app/src/main/java/com/example/questtracker/Goal.kt
@@ -0,0 +1,12 @@
+package com.example.questtracker
+
+data class Goal(
+    val id: Int,
+    var title: String,
+    var description: String,
+    var target: Int,
+    val creationDate: Date,
+    val deadline: Date,
+    var isComplete: Boolean,
+    var toDoTaskLink: ToDoTask?
+)
---
+++ app/src/main/java/com/example/questtracker/ToDoTask.kt
@@ -0,0 +1,12 @@
+package com.example.questtracker
+
+data class ToDoTask(
+    val id: Int,
+    var title: String,
+    var description: String,
+    var isRecurring: Boolean,
+    var recurrenceInterval: Int, // days
+    val deadline: Date,
+    var isComplete: Boolean,
+    var goalLink: Goal?
+)
